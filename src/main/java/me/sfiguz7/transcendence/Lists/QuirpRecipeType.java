3b87e45cb423af68c0a7f853d023ab7a2d3333d8
Sfiguz7 <ale.colli98@gmail.com>
2020-04-03 23:58:56 +0200

    Added quirps, quirp oscillator. Laid down some internal structure

---
+++ src/main/java/me/sfiguz7/transcendence/Lists/QuirpRecipeType.java
@@ -0,0 +1,23 @@
+package me.sfiguz7.transcendence.Lists;
+
+import me.mrCookieSlime.Slimefun.Lists.RecipeType;
+import me.mrCookieSlime.Slimefun.SlimefunPlugin;
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+
+public class QuirpRecipeType extends RecipeType {
+
+    public QuirpRecipeType(NamespacedKey key, SlimefunItemStack slimefunItem, String... lore) {
+        super(key, slimefunItem, null, lore);
+    }
+
+
+    public static final RecipeType QUIRPOSCILLATOR = new RecipeType(new NamespacedKey(SlimefunPlugin.instance, "quirposcillator"),
+            new CustomItem(Material.PURPUR_BLOCK, "&9Quirp Oscillator", "", "&a&oUse a Quirp Oscillator to obtain this Item")
+    );
+    public static final RecipeType QUIRPANNIHILATOR = new RecipeType(new NamespacedKey(SlimefunPlugin.instance, "quirpannihilator"),
+            new CustomItem(Material.SPONGE, "&9Quirp Annihilator", "", "&a&oUse a Quirp Annihilator to obtain this Item")
+    );
+}
--- src/main/java/me/sfiguz7/transcendence/TranscEndence.java
+++ src/main/java/me/sfiguz7/transcendence/TranscEndence.java
@@ -1,5 +1,16 @@
 package me.sfiguz7.transcendence;
 
+import io.github.thebusybiscuit.slimefun4.core.attributes.MachineTier;
+import io.github.thebusybiscuit.slimefun4.core.attributes.MachineType;
+import io.github.thebusybiscuit.slimefun4.utils.LoreBuilder;
+import me.mrCookieSlime.Slimefun.Lists.Categories;
+import me.mrCookieSlime.Slimefun.Lists.SlimefunItems;
+import me.mrCookieSlime.Slimefun.Objects.Research;
+import me.mrCookieSlime.Slimefun.SlimefunPlugin;
+import me.mrCookieSlime.Slimefun.api.Slimefun;
+import me.mrCookieSlime.Slimefun.cscorelib2.skull.SkullItem;
+import me.sfiguz7.transcendence.Lists.QuirpRecipeType;
+import me.sfiguz7.transcendence.implementation.quirps.generators.QuirpOscillator;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
@@ -14,7 +25,15 @@ import me.mrCookieSlime.Slimefun.bstats.bukkit.Metrics;
 import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
 import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
 
+import static me.sfiguz7.transcendence.Lists.TranscendenceItems.quirpOscillator;
+import static me.sfiguz7.transcendence.Lists.TranscendenceItems.quirpdown;
+import static me.sfiguz7.transcendence.Lists.TranscendenceItems.quirpleft;
+import static me.sfiguz7.transcendence.Lists.TranscendenceItems.quirpright;
+import static me.sfiguz7.transcendence.Lists.TranscendenceItems.quirpup;
+
 public class TranscEndence extends JavaPlugin implements SlimefunAddon {
+
+    private int researchId = 7100;
 
     @Override
     public void onEnable() {
@@ -29,11 +48,36 @@ public class TranscEndence extends JavaPlugin implements SlimefunAddon {
         new Metrics(this, bStatsId);
         */
 
-        Category category = new Category(new NamespacedKey(this, "transendence"),
-                new CustomItem(Material.PURPUR_BLOCK, "&5TranscEndence, ", "", "&a> Click to open")
+        Category transcendence = new Category(new NamespacedKey(this, "transcendence"),
+                new CustomItem(Material.PURPUR_BLOCK, "&5TranscEndence", "", "&a> Click to open")
         );
 
 
+        new SlimefunItem(transcendence, quirpup, QuirpRecipeType.QUIRPOSCILLATOR,
+                new ItemStack[]{null, null, null, null, null, null, null, null, null}
+        ).register(this);
+        new SlimefunItem(transcendence, quirpdown, QuirpRecipeType.QUIRPOSCILLATOR,
+                new ItemStack[]{null, null, null, null, null, null, null, null, null}
+        ).register(this);
+        new SlimefunItem(transcendence, quirpleft, QuirpRecipeType.QUIRPOSCILLATOR,
+                new ItemStack[]{null, null, null, null, null, null, null, null, null}
+        ).register(this);
+        new SlimefunItem(transcendence, quirpright, QuirpRecipeType.QUIRPOSCILLATOR,
+                new ItemStack[]{null, null, null, null, null, null, null, null, null}
+        ).register(this);
+
+        new QuirpOscillator(transcendence, quirpOscillator, RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{SlimefunItems.BLISTERING_INGOT_3, SlimefunItems.REINFORCED_PLATE, SlimefunItems.BLISTERING_INGOT_3,
+                        SlimefunItems.SYNTHETIC_EMERALD, SlimefunItems.NETHERSTAR_REACTOR, SlimefunItems.SYNTHETIC_EMERALD,
+                        SlimefunItems.BLISTERING_INGOT_3, SlimefunItems.REINFORCED_PLATE, SlimefunItems.BLISTERING_INGOT_3,}
+        ).register(this);
+
+        Slimefun.registerResearch(new Research(new NamespacedKey(this, "quirp_oscillator"),
+                        ++researchId,
+                        "Quirp_Oscillator",
+                        40),
+                quirpOscillator
+        );
 
     }
 
