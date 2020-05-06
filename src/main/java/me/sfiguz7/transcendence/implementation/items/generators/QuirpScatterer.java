edd411226b2d9653565156aae14a979f35c95d97
Sfiguz7 <ale.colli98@gmail.com>
2020-05-06 21:16:04 +0200

    Fixes #2

--- src/main/java/me/sfiguz7/transcendence/implementation/items/generators/QuirpScatterer.java
+++ src/main/java/me/sfiguz7/transcendence/implementation/items/generators/QuirpScatterer.java
@@ -13,7 +13,7 @@ public class QuirpScatterer extends AGenerator {
         super(TEItems.transcendence, TEItems.QUIRP_SCATTERER, TERecipeType.NANOBOT_CRAFTER,
                 new ItemStack[]{
                         TEItems.QUIRP_CONDENSATE, TEItems.QUIRP_UP, TEItems.QUIRP_CONDENSATE,
-                        TEItems.QUIRP_LEFT, TEItems.UNSTABLE_INGOT, TEItems.QUIRP_RIGHT,
+                        TEItems.QUIRP_LEFT, TEItems.QUIRP_CONDENSATE, TEItems.QUIRP_RIGHT,
                         TEItems.QUIRP_CONDENSATE, TEItems.QUIRP_DOWN, TEItems.QUIRP_CONDENSATE}
         );
     }
